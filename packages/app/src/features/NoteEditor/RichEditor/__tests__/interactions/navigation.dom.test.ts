1d8cb19677940184532080ba13002bbe897fbdc8
Irina Katsyuta <katsyuta@gmail.com>
2026-09-16 00:42:50 +0300

    fix: handle quote

--- packages/app/src/features/NoteEditor/RichEditor/__tests__/interactions/navigation.dom.test.ts
+++ packages/app/src/features/NoteEditor/RichEditor/__tests__/interactions/navigation.dom.test.ts
@@ -195,3 +195,78 @@ test('Cannot move blocks beyond document boundaries', async () => {
 	expect(paragraphsAfterDown[0]).toHaveTextContent('Green cup');
 	expect(paragraphsAfterDown[1]).toHaveTextContent('Red cup');
 });
+
+test('Moves whole list up', async () => {
+	const user = userEvent.setup();
+	await renderRichEditor({ value: 'Green cup \n\n - First item \n\n - Second item' });
+
+	const editor = screen.getByRole('textbox');
+	const paragraph = within(editor).getByRole('paragraph');
+	expect(paragraph).toHaveTextContent('Green cup');
+
+	const list = within(editor).getByRole('list');
+	expect(paragraph).toAppearBefore(list);
+
+	const items = within(editor).getAllByRole('listitem');
+	expect(items).toHaveLength(2);
+
+	await user.click(items[0]);
+	selectContent(editor, 'First item', 'Second item');
+	await user.keyboard('{Alt>}{ArrowUp}{/Alt}');
+
+	const paragraphAfterMove = within(editor).getByRole('paragraph');
+	expect(paragraphAfterMove).toHaveTextContent('Green cup');
+
+	const listAfterMove = within(editor).getByRole('list');
+	expect(paragraphAfterMove).toAppearAfter(listAfterMove);
+
+	const itemsAfterMove = within(editor).getAllByRole('listitem');
+	expect(itemsAfterMove).toHaveLength(2);
+	expect(itemsAfterMove[0]).toHaveTextContent('First item');
+	expect(itemsAfterMove[1]).toHaveTextContent('Second item');
+});
+
+test('Move nested blockquote', async () => {
+	const user = userEvent.setup();
+	await renderRichEditor({
+		value: `> Main quote
+>> Nested quote
+>>
+>> Again quote`,
+	});
+
+	const editor = screen.getByRole('textbox');
+	const quotes = within(editor).getAllByRole('blockquote');
+	expect(quotes).toHaveLength(2);
+
+	const nestedQuote = within(quotes[1]).getAllByRole('paragraph');
+
+	expect(nestedQuote).toHaveLength(2);
+	expect(nestedQuote[0]).toHaveTextContent('Nested quote');
+	expect(nestedQuote[1]).toHaveTextContent('Again quote');
+	expect(nestedQuote[0]).toAppearBefore(nestedQuote[1]);
+
+	await user.click(editor);
+	selectContent(editor, 'Again quote');
+	await user.keyboard('{Alt>}{ArrowUp}{/Alt}');
+
+	// Nested quote paragraphs should be reordered.
+	const quotesAfterMove = within(editor).getAllByRole('blockquote');
+	const nestedQuoteAfterMove = within(quotesAfterMove[1]).getAllByRole('paragraph');
+
+	expect(nestedQuoteAfterMove).toHaveLength(2);
+	expect(nestedQuoteAfterMove[0]).toHaveTextContent('Again quote');
+	expect(nestedQuoteAfterMove[1]).toHaveTextContent('Nested quote');
+	expect(nestedQuoteAfterMove[0]).toAppearBefore(nestedQuoteAfterMove[1]);
+
+	// Moving up again should have no effect.
+	await user.keyboard('{Alt>}{ArrowUp}{/Alt}');
+
+	const quotesAfterNoop = within(editor).getAllByRole('blockquote');
+	const nestedQuoteAfterNoop = within(quotesAfterNoop[1]).getAllByRole('paragraph');
+
+	expect(nestedQuoteAfterNoop).toHaveLength(2);
+	expect(nestedQuoteAfterNoop[0]).toHaveTextContent('Again quote');
+	expect(nestedQuoteAfterNoop[1]).toHaveTextContent('Nested quote');
+	expect(nestedQuoteAfterNoop[0]).toAppearBefore(nestedQuoteAfterNoop[1]);
+});
